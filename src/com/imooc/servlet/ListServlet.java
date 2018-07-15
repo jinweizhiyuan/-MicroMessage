1a7411fc4775f7d135c94bba1279981bc7b4c7a8
unknown <jinwei_zhiyuan@163.com>
2018-07-15 22:58:10 +0800

    init

---
+++ src/com/imooc/servlet/ListServlet.java
@@ -0,0 +1,30 @@
+package com.imooc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author hqlip
+ * 列表页面初始化控制
+ *
+ */
+@SuppressWarnings("serial")
+public class ListServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(req, resp);
+	}
+}
