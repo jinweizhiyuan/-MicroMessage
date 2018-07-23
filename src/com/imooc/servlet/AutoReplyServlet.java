2c56b51f176fd3905b6214306505c09bf52ebd81
unknown <jinwei_zhiyuan@163.com>
2018-07-23 23:09:21 +0800

    talk.jsp

---
+++ src/com/imooc/servlet/AutoReplyServlet.java
@@ -0,0 +1,50 @@
+package com.imooc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.imooc.service.QueryService;
+
+/**
+ * Servlet implementation class AutoReplyServlet
+ */
+@WebServlet("/AutoReplyServlet.action")
+public class AutoReplyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AutoReplyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		QueryService queryService = new QueryService();
+		out.print(queryService.queryByCommand(request.getParameter("content")));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+
+}
---
+++ src/com/imooc/util/Iconst.java
@@ -0,0 +1,7 @@
+package com.imooc.util;
+
+public interface Iconst {
+	
+	public static String NO_MACTHING__CONTENT = "客官，你没有按套路出牌!";
+
+}
